bc21e19834e12f5e61ab66ad27c5472830e454a1
Volldy <31956783+Volldy@users.noreply.github.com>
2017-11-30 14:13:01 +0100

    Add files via upload

---
+++ pochopeni_funkci.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 30 13:40:18 2017
+
+@author: vol35098
+"""
+hra = input('Chceš si zahrát OKO? (Ano/Ne) : ')
+while hra == 'Ne':
+    print('Konec hry')
+    if hra == 'Ano':
+       
+    
+
+    
+
+        
+        
+        
